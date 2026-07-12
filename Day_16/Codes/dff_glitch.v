e129dbc25cf824e2d27a13c705fb92129e4d88cb
bharath <myasuslaptop45@gmail.com>
2026-07-12 11:30:12 +0530

    Create dff_glitch.v

---
+++ Day_16/Codes/dff_glitch.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+
+module dff_glitch(
+    input clk,
+    input rst,
+    input D,
+    output reg Q,
+    output Qb
+);
+
+always @(posedge clk) begin
+  
+    if (rst)
+      
+        Q <= 1'b0;
+  
+    else
+      
+        Q <= D;
+  
+end
+
+assign Qb = ~Q;
+
+endmodule
