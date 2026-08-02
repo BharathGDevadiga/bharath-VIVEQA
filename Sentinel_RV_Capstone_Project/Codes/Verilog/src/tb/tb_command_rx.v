e1d59e33a28310bcf6f8a28b46b7b64b3d6678c6
BharathGDevadiga <myasuslaptop45@gmail.com>
2026-08-02 10:15:53 +0530

    Fix inline variable initialization in tb_command_rx.v for strict Verilog compilers

--- Sentinel_RV_Capstone_Project/Codes/Verilog/src/tb/tb_command_rx.v
+++ Sentinel_RV_Capstone_Project/Codes/Verilog/src/tb/tb_command_rx.v
@@ -1,7 +1,7 @@
 `timescale 1ns/1ps
 module tb_command_rx;
-    reg clk = 0, reset = 1, rx_valid = 0;
-    reg [7:0] rx_data = 0;
+    reg clk, reset, rx_valid;
+    reg [7:0] rx_data;
     wire cmd_valid, cmd_error;
     wire [7:0] opcode, seq_num;
     wire [15:0] argument;
@@ -30,6 +30,11 @@ module tb_command_rx;
     endtask
 
     initial begin
+        clk = 0;
+        reset = 1;
+        rx_valid = 0;
+        rx_data = 0;
+        
         repeat (2) @(posedge clk);
         reset = 0;
 
